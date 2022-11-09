3a90df67dbaf2ce6c183aa5300f5e85dd560fad6
Nguyễn Đình Thuyết <646372@sv.vnua.edu.vn>
2022-11-09 13:04:28 +0700

    Chỉnh sửa api lần 2

--- VNUA.FITA.MQTT.HRM/VNUA.FITA.MQTT.HRM.Biz/BaiViet/Repository.cs
+++ VNUA.FITA.MQTT.HRM/VNUA.FITA.MQTT.HRM.Biz/BaiViet/Repository.cs
@@ -1,10 +1,37 @@
-﻿using System;
+﻿using AutoMapper;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using VNUA.FITA.MQTT.HRM.Biz.Model.BaiViet;
+using Microsoft.EntityFrameworkCore;
+using VNUA.FITA.MQTT.HRM.Data.Access;
 
 namespace VNUA.FITA.MQTT.HRM.Biz.BaiViet
 {
-    class Repository
+    public class Repository : IRepository
     {
+        private readonly SQLServerDbContext _context;
+        private readonly IMapper _mapper;
+
+        public Task<View> CreatAsync(New model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<View> GetAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<View> UpdateAsync(Edit model)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
