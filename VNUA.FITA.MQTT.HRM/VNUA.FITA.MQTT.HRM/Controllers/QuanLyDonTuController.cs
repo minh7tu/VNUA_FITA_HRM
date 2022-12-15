296f9d027d400b55395ca8bf8949c8b668b6d527
Nguyễn Đình Thuyết <646372@sv.vnua.edu.vn>
2022-12-15 22:22:13 +0700

    Tú - Đẩy 3 controller quản lý

---
+++ VNUA.FITA.MQTT.HRM/VNUA.FITA.MQTT.HRM/Controllers/QuanLyDonTuController.cs
@@ -0,0 +1,182 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VNUA.FITA.MQTT.HRM.Data.Access;
+using VNUA.FITA.MQTT.HRM.Data.Model;
+
+namespace VNUA.FITA.MQTT.HRM.Controllers
+{
+    public class QuanLyDonTuController : Controller
+    {
+        private readonly SqlServerDbContext _context;
+
+        public QuanLyDonTuController(SqlServerDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: QLDonTu
+        public async Task<IActionResult> Index()
+        {
+            ViewBag.SessionUser = HttpContext.Session.GetString("SessionUser");
+            ViewBag.SessionImage = HttpContext.Session.GetString("SessionImage");
+            ViewBag.ChucVu = HttpContext.Session.GetString("SessionChucVu");
+            var sqlServerDbContext = _context.DonTus.Include(d => d.NhanViens);
+            return View(await sqlServerDbContext.ToListAsync());
+        }
+
+        // GET: QLDonTu/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            ViewBag.SessionUser = HttpContext.Session.GetString("SessionUser");
+            ViewBag.SessionImage = HttpContext.Session.GetString("SessionImage");
+            ViewBag.ChucVu = HttpContext.Session.GetString("SessionChucVu");
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var donTu = await _context.DonTus
+                .Include(d => d.NhanViens)
+                .FirstOrDefaultAsync(m => m.IdDonTu == id);
+            if (donTu == null)
+            {
+                return NotFound();
+            }
+
+            return View(donTu);
+        }
+
+        // GET: QLDonTu/Create
+        public IActionResult Create()
+        {
+            ViewBag.SessionUser = HttpContext.Session.GetString("SessionUser");
+            ViewBag.SessionImage = HttpContext.Session.GetString("SessionImage");
+            ViewBag.ChucVu = HttpContext.Session.GetString("SessionChucVu");
+            ViewData["IdNhanVien"] = new SelectList(_context.NhanViens, "IdNhanVien", "IdNhanVien");
+            return View();
+        }
+
+        // POST: QLDonTu/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IdDonTu,TieuDe,NoiDung,TrangThai,GhiChu,NguoiNhan,PhanLoai,ThoiGian,IdNhanVien")] DonTu donTu)
+        {
+            ViewBag.SessionUser = HttpContext.Session.GetString("SessionUser");
+            ViewBag.SessionImage = HttpContext.Session.GetString("SessionImage");
+            ViewBag.ChucVu = HttpContext.Session.GetString("SessionChucVu");
+            if (ModelState.IsValid)
+            {
+                _context.Add(donTu);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdNhanVien"] = new SelectList(_context.NhanViens, "IdNhanVien", "IdNhanVien", donTu.IdNhanVien);
+            return View(donTu);
+        }
+
+        // GET: QLDonTu/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            ViewBag.SessionUser = HttpContext.Session.GetString("SessionUser");
+            ViewBag.SessionImage = HttpContext.Session.GetString("SessionImage");
+            ViewBag.ChucVu = HttpContext.Session.GetString("SessionChucVu");
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var donTu = await _context.DonTus.FindAsync(id);
+            if (donTu == null)
+            {
+                return NotFound();
+            }
+            ViewData["IdNhanVien"] = new SelectList(_context.NhanViens, "IdNhanVien", "IdNhanVien", donTu.IdNhanVien);
+            return View(donTu);
+        }
+
+        // POST: QLDonTu/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("IdDonTu,TieuDe,NoiDung,TrangThai,GhiChu,NguoiNhan,PhanLoai,ThoiGian,IdNhanVien")] DonTu donTu)
+        {
+            ViewBag.SessionUser = HttpContext.Session.GetString("SessionUser");
+            ViewBag.SessionImage = HttpContext.Session.GetString("SessionImage");
+            ViewBag.ChucVu = HttpContext.Session.GetString("SessionChucVu");
+            if (id != donTu.IdDonTu)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(donTu);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!DonTuExists(donTu.IdDonTu))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdNhanVien"] = new SelectList(_context.NhanViens, "IdNhanVien", "IdNhanVien", donTu.IdNhanVien);
+            return View(donTu);
+        }
+
+        // GET: QLDonTu/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            ViewBag.SessionUser = HttpContext.Session.GetString("SessionUser");
+            ViewBag.SessionImage = HttpContext.Session.GetString("SessionImage");
+            ViewBag.ChucVu = HttpContext.Session.GetString("SessionChucVu");
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var donTu = await _context.DonTus
+                .Include(d => d.NhanViens)
+                .FirstOrDefaultAsync(m => m.IdDonTu == id);
+            if (donTu == null)
+            {
+                return NotFound();
+            }
+
+            return View(donTu);
+        }
+
+        // POST: QLDonTu/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var donTu = await _context.DonTus.FindAsync(id);
+            _context.DonTus.Remove(donTu);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool DonTuExists(int id)
+        {
+            return _context.DonTus.Any(e => e.IdDonTu == id);
+        }
+    }
+}
